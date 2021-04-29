5b5a1ecef8f26ba22e9e52a76e935b259629f669
DamienCarreau <verybadchti@gmail.com>
2021-04-29 10:40:12 +0200

    loading fixed

--- Assets/mapLoader.cs
+++ Assets/mapLoader.cs
@@ -10,6 +10,7 @@ public class mapLoader : MonoBehaviour
     public TextMesh logText;
     List<GameObject> maps;
     int xOffset, zOffset;
+    int xMin,xMax,zMin,zMax;
 
     private MapRenderer basicMapRenderer;
 
@@ -28,6 +29,10 @@ public class mapLoader : MonoBehaviour
                 maps.Add(tile);
             }
         }
+        zMin = -size;
+        zMax = size;
+        xMin = -size;
+        xMax = size;
         xOffset = 0;
         zOffset = 0;
     }
@@ -38,15 +43,15 @@ public class mapLoader : MonoBehaviour
         //transform.position += new Vector3(-0.005f,0f,0.002f);
         Vector3 camera = transform.position;
 
-        log("xOffset : "+xOffset+" - zOffset : "+zOffset);
-
         //check if you're changing of row (X axe)
         if(Mathf.FloorToInt ((camera.x+1.5f)/3f) != xOffset){
+        	int direction = Mathf.FloorToInt ((camera.x+1.5f)/3f) - xOffset;
+        	xMax += direction;
+        	xMin += direction;
         	xOffset = Mathf.FloorToInt ((camera.x+1.5f)/3f);
-        	float xPos = camera.x - Mathf.Sign(xOffset) * (size+Mathf.Abs(xOffset)) * 3f;
         	for(int j = -size; j <= size; j++)
         	{  
-                GameObject tile = GameObject.Instantiate(basicMap, new Vector3(xPos, camera.y, camera.z - Mathf.Sign(zOffset) * (j+Mathf.Abs(zOffset)) * 3f), Quaternion.Euler(0, 0, 0), this.transform);
+                GameObject tile = GameObject.Instantiate(basicMap, new Vector3(camera.x - (direction>0?xMax:xMin)*3f, camera.y, camera.z - Mathf.Sign(zOffset) * (j+Mathf.Abs(zOffset)) * 3f), Quaternion.Euler(0, 0, 0), this.transform);
                 TransformWorldPointToLatLon(tile);
                 maps.Add(tile);
         	}
@@ -54,8 +59,8 @@ public class mapLoader : MonoBehaviour
         	// Selecting all the maps to delete
         	foreach(GameObject map in maps)
         	{
-        		if( (xOffset >= 0 && map.transform.position.x > -xPos) ||
-    				(xOffset <  0 && map.transform.position.x < -xPos)){
+        		if( (direction >= 0 && map.transform.position.x > -camera.x + xMax*3f) ||
+    				(direction <  0 && map.transform.position.x < -camera.x + xMin*3f)){
         			toDestroy.Add(map);
         		}
         	}
@@ -68,19 +73,21 @@ public class mapLoader : MonoBehaviour
 
         //check if you're changing of column (Z axe)
         if(Mathf.FloorToInt ((camera.z+1.5f)/3f) != zOffset){
+        	int direction = Mathf.FloorToInt ((camera.z+1.5f)/3f) - zOffset;
+        	zMax += direction;
+        	zMin += direction;
         	zOffset = Mathf.FloorToInt ((camera.z+1.5f)/3f);
-        	float zPos = camera.z - Mathf.Sign(zOffset) * (size+Mathf.Abs(zOffset)) * 3f;
         	for(int i = -size; i <= size; i++)
         	{  
-                GameObject tile = GameObject.Instantiate(basicMap, new Vector3(camera.x - Mathf.Sign(xOffset) * (i+Mathf.Abs(xOffset)) * 3f, camera.y, zPos), Quaternion.Euler(0, 0, 0), this.transform);
+                GameObject tile = GameObject.Instantiate(basicMap, new Vector3(camera.x - Mathf.Sign(xOffset) * (i+Mathf.Abs(xOffset)) * 3f, camera.y, camera.z - (direction>0?zMax:zMin)*3f), Quaternion.Euler(0, 0, 0), this.transform);
                 TransformWorldPointToLatLon(tile);
                 maps.Add(tile);
         	}
         	List<GameObject> toDestroy = new List<GameObject>();
         	foreach(GameObject map in maps)
         	{
-        		if( (zOffset >= 0 && map.transform.position.z > -zPos) ||
-    				(zOffset <  0 && map.transform.position.z < -zPos)){
+        		if( (direction >= 0 && map.transform.position.z > -camera.z + zMax*3f) ||
+    				(direction <  0 && map.transform.position.z < -camera.z + zMin*3f)){
         			toDestroy.Add(map);
         		}
         	}
