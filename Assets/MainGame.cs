a4810925974b8b959ef445cdf736c1d96ec670dd
TimotheBerthier <berthiertimothe@gmail.com>
2021-04-30 17:04:15 +0200

    city menu fixed

--- Assets/MainGame.cs
+++ Assets/MainGame.cs
@@ -9,6 +9,7 @@ public class MainGame : MonoBehaviour
     
     public void PauseGame()
     {
+        Debug.Log("pause activated");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
     }
     
