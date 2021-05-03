cb4ed7876c88b4f60f86d17abf8c2f6ff2c8b780
DamienCarreau <carreau.damien@gmail.com>
2021-05-03 17:36:05 +0200

    direction data

--- Assets/Script/click.cs
+++ Assets/Script/click.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.XR;
 
-public class InputManager : MonoBehaviour
+public class click : MonoBehaviour
 {
     [SerializeField]
     private XRNode xRNode = XRNode.RightHand;
