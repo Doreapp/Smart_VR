02eb025d8cb5376dca4c3ec0ed280b3164a82595
DamienCarreau <carreau.damien@gmail.com>
2021-05-03 17:15:34 +0200

    getTrigger

---
+++ Assets/Script/click.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private XRNode xRNode = XRNode.RightHand;
+
+    private List<InputDevice> devices = new List<InputDevice>();
+
+    private InputDevice device;
+
+    //to avoid repeat readings
+    private bool triggerIsPressed;
+
+    public TextMesh logText;
+
+    void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(xRNode, devices);
+        device = devices.FirstOrDefault();
+    }
+
+    void OnEnable()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+    }
+
+    void Update()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+
+        // capturing trigger button press and release    
+        bool triggerButtonValue = false;
+        if (device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerButtonValue) && triggerButtonValue && !triggerIsPressed)
+        {
+            triggerIsPressed = true;
+            log("True");
+
+        }
+        else if (!triggerButtonValue && triggerIsPressed)
+        {
+            triggerIsPressed = false;
+            log("false");
+        }
+    }
+
+    private void log(string str) {
+        logText.text = str;
+    }
+}
