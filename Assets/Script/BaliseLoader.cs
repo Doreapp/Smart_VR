54fc1785be515942a004877badc4c1eb3ab60ddc
TimotheBerthier <berthiertimothe@gmail.com>
2021-05-04 15:29:19 +0200

    conflicts fixed

--- Assets/Script/BaliseLoader.cs
+++ Assets/Script/BaliseLoader.cs
@@ -25,12 +25,9 @@ public class BaliseLoader : MonoBehaviour
     public MapRenderer basicMapRenderer;
     public TextMesh basicText;
     public TextAsset file;
-<<<<<<< HEAD
+    public Compass compass;
     public float yBalise;
     public int mode;
-=======
-    public Compass compass;
->>>>>>> boussolebis
 
     // Start is called before the first frame update
     void Start()
@@ -41,40 +38,6 @@ public class BaliseLoader : MonoBehaviour
         //TextAsset textAsset = Resources.Load<TextAsset>("../Maps/NewYork.json");
         Map map = JsonUtility.FromJson<Map>(file.text);
 
-<<<<<<< HEAD
-    	switch (mode) {
-    		case 0 :
-    			// No balises
-    			break;
-    		case 1 :
-    			// Balises from a json, points of interest
-		        foreach(Balise b in map.balises){
-		            // Creating the balise
-		            GameObject balise = Instantiate(basicBalise, balises.transform);
-		            Vector3 pos = MapRendererTransformExtensions.TransformLatLonAltToLocalPoint(basicMapRenderer, new LatLonAlt(b.latitude, b.longitude, 0));
-		            // Set the position of the balise y above the map
-		            balise.transform.position = pos + new Vector3(0f,yBalise,0f);
-		            // Creating the text as a child of the balise
-		            TextMesh description = Instantiate(basicText, balise.transform);
-		            description.text = b.nom;
-		            // Set the position of the text y+1 above the map
-		            description.transform.position = pos + new Vector3(0f,yBalise+1,0f);
-		        }
-		    	break;	
-	    	case 2 :
-	    		// 20 Randoms Balises
-	    		float delta = 0.5f;
-	    		for (int i = 0; i < 20; i++){
-	    			float latitude = map.balises[0].latitude + UnityEngine.Random.Range(-delta, delta);
-	    			float longitude = map.balises[0].longitude + UnityEngine.Random.Range(-delta, delta);
-	    			GameObject balise = Instantiate(basicBalise, balises.transform);
-		            Vector3 pos = MapRendererTransformExtensions.TransformLatLonAltToLocalPoint(basicMapRenderer, new LatLonAlt(latitude, longitude, 0));
-	    			// Set the position of the balise y above the map
-		            balise.transform.position = pos + new Vector3(0f,yBalise,0f);
-	    		}
-	    		break;
-	    }
-=======
         foreach(Balise b in map.balises){
             // Creating the balise
             GameObject balise = Instantiate(basicBalise, balises.transform);
@@ -86,10 +49,7 @@ public class BaliseLoader : MonoBehaviour
             description.text = b.nom;
             // Set the position of the text 2 above the map
             description.transform.position = pos + new Vector3(0f,2f,0f);
-            //Add the balise to the compass
-            compass.AddMarker(balise);
         }
->>>>>>> boussolebis
     }
 
     // Update is called once per frame
