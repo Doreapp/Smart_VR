ab8cf6b324ac1bb308356e56484dff467e00fa6a
DamienCarreau <carreau.damien@gmail.com>
2021-05-04 11:16:30 +0200

    mode 2 balises

--- Assets/Script/BaliseLoader.cs
+++ Assets/Script/BaliseLoader.cs
@@ -25,29 +25,45 @@ public class BaliseLoader : MonoBehaviour
     public MapRenderer basicMapRenderer;
     public TextMesh basicText;
     public TextAsset file;
-    public float y;
+    public float yBalise;
+    public int mode;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject balises = new GameObject("Balises");
+    	GameObject balises = new GameObject("Balises");
         balises.transform.SetParent(this.transform);
 
         //TextAsset textAsset = Resources.Load<TextAsset>("../Maps/NewYork.json");
         Map map = JsonUtility.FromJson<Map>(file.text);
 
-        foreach(Balise b in map.balises){
-            // Creating the balise
-            GameObject balise = Instantiate(basicBalise, balises.transform);
-            Vector3 pos = MapRendererTransformExtensions.TransformLatLonAltToLocalPoint(basicMapRenderer, new LatLonAlt(b.latitude, b.longitude, 0));
-            // Set the position of the balise 1 above the map
-            balise.transform.position = pos + new Vector3(0f,y,0f);
-            // Creating the text as a child of the balise
-            TextMesh description = Instantiate(basicText, balise.transform);
-            description.text = b.nom;
-            // Set the position of the text 2 above the map
-            description.transform.position = pos + new Vector3(0f,y+1,0f);
-        }
+    	switch (mode) {
+    		case 1 :
+		        foreach(Balise b in map.balises){
+		            // Creating the balise
+		            GameObject balise = Instantiate(basicBalise, balises.transform);
+		            Vector3 pos = MapRendererTransformExtensions.TransformLatLonAltToLocalPoint(basicMapRenderer, new LatLonAlt(b.latitude, b.longitude, 0));
+		            // Set the position of the balise y above the map
+		            balise.transform.position = pos + new Vector3(0f,yBalise,0f);
+		            // Creating the text as a child of the balise
+		            TextMesh description = Instantiate(basicText, balise.transform);
+		            description.text = b.nom;
+		            // Set the position of the text y+1 above the map
+		            description.transform.position = pos + new Vector3(0f,yBalise+1,0f);
+		        }
+		    	break;	
+	    	case 2 :
+	    		float delta = 0.5f;
+	    		for (int i = 0; i < 20; i++){
+	    			float latitude = map.balises[0].latitude + UnityEngine.Random.Range(-delta, delta);
+	    			float longitude = map.balises[0].longitude + UnityEngine.Random.Range(-delta, delta);
+	    			GameObject balise = Instantiate(basicBalise, balises.transform);
+		            Vector3 pos = MapRendererTransformExtensions.TransformLatLonAltToLocalPoint(basicMapRenderer, new LatLonAlt(latitude, longitude, 0));
+	    			// Set the position of the balise 1 above the map
+		            balise.transform.position = pos + new Vector3(0f,yBalise,0f);
+	    		}
+	    		break;
+	    }
     }
 
     // Update is called once per frame
